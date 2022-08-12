259905d4f35355791467e3f74cd24ab844a27e3c
Francisco Quagliotti <quagliottifrancisco@gmail.com>
2022-08-12 14:47:57 -0300

    made Productos page more responsive

--- js/products.js
+++ js/products.js
@@ -26,16 +26,16 @@ function showProducts(array) {
         let htmlContentToAppend = `
         <div class="list-group-item list-group-item-action">
             <div class="row">
-                <div class="col-3">
+                <div class="col-md-3">
                     <img src="${element.image}" alt="product image" class="img-thumbnail">
                 </div>
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
-                        <div class="mb-1">
+                        <div class="p-1">
                         <h4>${element.name} - ${element.currency} ${element.cost}</h4> 
                         <p>${element.description}</p> 
                         </div>
-                        <small class="text-muted">${element.soldCount} vendidos</small> 
+                        <small class="text-muted p-1">${element.soldCount} vendidos</small> 
                     </div>
                 </div>
             </div>
