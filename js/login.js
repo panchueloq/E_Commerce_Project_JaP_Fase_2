9ca0d82b75e3887086789a1d597544715ce782f2
Francisco Quagliotti <quagliottifrancisco@gmail.com>
2022-08-17 15:25:15 -0300

    added login features

--- js/login.js
+++ js/login.js
@@ -3,15 +3,13 @@ const password = document.getElementById('password');
 const login = document.getElementById('login_btn');
 const google = document.getElementById('google_btn');
 
-login.addEventListener('click', function(event){
+login.addEventListener('click', ()=>{
     event.preventDefault();
     if (email.value == '' || password.value == ''){
         alert('Por favor, completar los campos de ingreso');
     }
     else {
-        console.log('go ahead');
         window.location.href = './home.html';
     }
 })
-
 
