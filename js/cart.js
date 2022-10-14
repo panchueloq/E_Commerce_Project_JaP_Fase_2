63ff696f4569ebf2e4bf8086bd04f21fbb97a292
Francisco Quagliotti <quagliottifrancisco@gmail.com>
2022-10-14 11:16:36 -0300

    started to build cart page

---
+++ js/cart.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+`<div class="row mt-2 align-items-center">
+    <div class="col-2 d-none d-md-block text-center">
+        <img src="https://placekitten.com/60/60" class="img-thumbnail" style="max-width: 60px; max-height: 60px;">
+    </div>
+    <div class="col-3 col-md-3">Peugeot 208</div>
+    <div class="col-3 col-md-2">USD 25000</div>
+    <div class="col-3 col-md-2"><input type="number" min ="1" style="max-width: 70px;"></div>
+    <div class="col-3 col-md-3"><b>USD 25000</b></div>
+</div>`
