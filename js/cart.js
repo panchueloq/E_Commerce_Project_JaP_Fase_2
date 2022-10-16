083ee8aefa8ed3e4051e79686606238f57c2016d
Francisco Quagliotti <quagliottifrancisco@gmail.com>
2022-10-16 12:36:27 -0300

    added subtotal capabilities to cart

--- js/cart.js
+++ js/cart.js
@@ -39,13 +39,13 @@ function add_from_LS(){
     let content = '';
     cart_items.forEach(item => {
         content += `
-        <div class="item_div row mt-2 align-items-center">
+        <div class="item_div row mt-2 align-items-center" id="${item.id}">
             <div class="col-2 d-none d-md-block text-center">
                 <img src="${item.image}" style="max-width: 60px; max-height: 60px;">
             </div>
             <div class="col-3 col-md-3">${item.name}</div>
             <div class="cost_div col-3 col-md-2">${item.currency} <span>${item.unitCost}</span></div>
-            <div class="input_div col-3 col-md-2"><input type="number" min ="1" value="${item.count}" style="max-width: 70px;"></div>
+            <div class="input_div col-3 col-md-2"><input type="number" min ="1" value="${item.count}" style="max-width: 70px;" onchange="change_count(${item.id}); check_positive_input(${item.id});"></div>
             <div class="span_div col-3 col-md-3"><b>${item.currency} <span>${item.unitCost*item.count}</span></b></div>
         </div>
         <hr class="p-0 m-0 mt-1">` 
@@ -54,19 +54,35 @@ function add_from_LS(){
 }
 
 // function to check positive value of "Cantidad" input
-function check_positive_input(){
-    let items = document.querySelectorAll('.item_div');
-    items.forEach(element => {
-        var input = element.querySelector('.input_div > input');
-        input.addEventListener('change', ()=>{
-            if (element.querySelector('.input_div > input').value < 1){
-                element.querySelector('.input_div > input').value = 1
+function check_positive_input(index){
+    let item = document.getElementById(`${index}`);
+    cart_items = cart_items.map(object =>{
+        if (object.id === index){
+            if(item.querySelector('.input_div > input').value < 1){
+                object.count = 1;
             }
-        })
+        }
+        return object;
     })
+    localStorage.setItem('cart_items_LS', JSON.stringify(cart_items));
+    add_from_LS();
 }
 
-
+// function to change count depending on "Cantidad" input
+function change_count(index){
+    
+    let item = document.getElementById(`${index}`);
+    if(item.querySelector('.input_div > input').value > 0){
+        cart_items = cart_items.map(object => {
+            if (object.id === index){
+                object.count = item.querySelector('.input_div > input').value;
+            }
+            return object;
+        });
+        localStorage.setItem('cart_items_LS', JSON.stringify(cart_items));
+        add_from_LS();
+    }
+}
 
 
 
