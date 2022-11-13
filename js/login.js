d6d256316c9c21f18777e40df08c6419c18d1bee
Francisco Quagliotti <quagliottifrancisco@gmail.com>
2022-11-13 14:44:12 -0300

    updated login

--- js/login.js
+++ js/login.js
@@ -1,41 +1,31 @@
-const login = document.getElementById('login_btn');
-const inputs = document.querySelectorAll('form input');
-var valid = true;
-var personal_email;
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+    
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+var forms = document.querySelectorAll('.needs-validation')
 
-login.addEventListener('click', (event)=>{
-    event.preventDefault();
-    valid = true;
-    cleanErrors(inputs);
-    verifyEmpty(inputs);
-    signIn(valid);
-})
-
-function verifyEmpty(array){
-    array.forEach(element => {
-        if (element.value == '' || element.value == null){
-            let errormsg = document.querySelector(`#${element.id}+p`);
-            errormsg.innerHTML = `Por favor, ingrese su ${element.name}`;
-            valid = false;
+// Loop over them and prevent submission
+Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+    form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
         }
-    });
-}
+        else{
+            event.preventDefault()
+            event.stopPropagation()
+            completeLogin()
+            }
 
-function signIn(valid){
-    if (valid){
-        setEmail();
-        localStorage.setItem('personal_email', personal_email)
-        window.location.href = './home.html';
-    }
-}
+        form.classList.add('was-validated')
+        }, false)
+    })
+})()
 
-function cleanErrors(array){
-    array.forEach(element => {
-        let errormsg = document.querySelector(`#${element.id}+p`);
-        errormsg.innerHTML = '';
-    });
-}
-
-function setEmail(){
-    personal_email = document.getElementById('email').value;
+function completeLogin(){
+    let personal_email = document.getElementById('email').value;
+    localStorage.setItem('personal_email', personal_email);
+    window.location.href = './home.html';
 }
